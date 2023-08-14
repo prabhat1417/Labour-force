0a1778a8600203b0ef6ab6f52b07d88eca125979
prabhat1417 <106381748+prabhat1417@users.noreply.github.com>
2023-08-14 13:59:48 +0530

    Edit Card to show user data

--- src/components/CategoryRow/index.js
+++ src/components/CategoryRow/index.js
@@ -1,31 +1,46 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import "./index.css";
-import dummyData from "./dummy.json";
 
-function ProfileCardRow() {
+const ProfileCardRow = (userInfo) => {
+  const [userLoaded, setUserLoaded] = useState(false);
+  const user = userInfo.userInfo.userInfo;
+  console.log(user);
+  useEffect(() => {
+    const delay = 2000;
+    const timer = setTimeout(() => {
+      setUserLoaded(true);
+    }, delay);
+
+    return () => clearTimeout(timer);
+
+  }, []);
+
+  if (!userLoaded) {
+    return null; 
+  }
   return (
     <>
     <div className="profile-card-row">
-      {dummyData.map((profile) => (
-        <div className="row-card-container" key={profile.name}>
+      {user.map((profile,index) => (
+        <div className="row-card-container" key={index}>
           <header className="row-back">
             <img className="row-logo" src={'https://www.epicscotland.com/wp-content/uploads/2018/01/Business-Headshot_002.jpg'} alt={profile.name} />
           </header>
           <h1 className="row-bold-text">
-            {profile.name}
-          </h1>
-          <h2 className="row-normal-text">{profile.profession}</h2>
+            {profile.Name}
+          </h1>   
+          <h2 className="row-normal-text">{profile.city}</h2>
           <div className="row-social-container">
             <div className="row-followers">
-              <h1 className="row-bold-text">{profile.rating}</h1>
-              <h2 className="row-smaller-text">Ratings</h2>
-            </div>
+              <h1 className="row-bold-text">{profile.Experience}</h1>
+              <h2 className="row-smaller-text">Experience</h2>
+            </div>  
             <div className="row-likes">
               <h1 className="row-bold-text">{profile.skills.length}</h1>
               <h2 className="row-smaller-text">Skills</h2>
             </div>
             <div className="row-photos">
-              <h1 className="row-bold-text">{profile.price}</h1>
+              <h1 className="row-bold-text">{profile.skills[0].price}</h1>
               <h2 className="row-smaller-text">Price/h</h2>
             </div>
           </div>
