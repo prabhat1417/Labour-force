e7d9b9dce87dbb16bb9ed2403ad4faa8014b512d
rohit <rohitchouhan3512@gmail.com>
2023-08-15 02:14:26 +0530

    fixed dashboard bug

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,20 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import './Dashboard.css';
-import { auth } from '../../firebase';
+import { auth,db } from '../../firebase';
 import CustomerTable from './CustomerTable';
+import { collection,getDocs,doc, query,where } from 'firebase/firestore';
+
 
 const Dashboard = ({userInfo}) => {
 
     const [currUser, setCurrUser] = useState(null);
     const [currWorker, setCurrWorker] = useState({});
     const [currEmail, setCurrEmail] = useState("");
-    const [userLoaded, setUserLoaded] = useState(false);
 
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(async (user) => {
-            
             if (user) {
-                console.log(user);
+                // console.log(user);
                 setCurrUser({
                     email: user.email,
                     displayName: user.displayName,
@@ -22,22 +22,36 @@ const Dashboard = ({userInfo}) => {
                 });
     
                 setCurrEmail(user.email);
+                const CollectionRef=collection(db,"memberships");
+                const querySnapshot=await getDocs(query(CollectionRef,where("email","==",user.email)));
+                if(!querySnapshot.empty){
+                    const customer=querySnapshot.docs[0].data();
+                    console.log("customer:",customer);
+                }
+              
     
                 const foundWorker = userInfo.find(worker => worker.email === user.email);
                 if (foundWorker) {
-                    foundWorker.customers = Array.from({ length: 10 }, (_, index) => `customer${index + 1}@example.com`);
+                    let i = 0; let j = 1;
+                    for(i=0; i<10000; i++) {
+                        j += 1;
+                    }
+                    setTimeout(()=>{
+
+                    },0)
+                    foundWorker.customers = Array.from({ length: 1 }, (_, index) => `customer${index + 1}@example.com`);
                     setCurrWorker(foundWorker);
                 }
             } else {
                 setCurrUser(null);
             }
         });
-            
         return () => unsubscribe();
     }, []);
+    
 
-    if(currUser?.customers) {
-        console.log("currUser ", currUser);
+    if(currUser) {
+        console.log("currUser ", currWorker);
     }
 
   return (
